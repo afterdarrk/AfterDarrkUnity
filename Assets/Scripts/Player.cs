1c3ded816007ec0663ba73d4b228d23a3d6ec4f1
James Dalboth <James.Dalboth@gmail.com>
2019-08-24 17:21:35 +0100

    Add Player Object
    * Add Player object and enemies move towards player and die on touch
    
    * Clean up style issues

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    // Use this for initialization
+    void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		// Closes the application (Note: in unity editor nothing happens)
+		Destroy (col.gameObject);
+		Debug.Log("Closing Application");
+	}
+}
