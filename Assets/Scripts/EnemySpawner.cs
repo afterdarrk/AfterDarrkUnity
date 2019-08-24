2d9d09cddd732dfe10944bdc27b8c7928afde3f7
James Dalboth <James.Dalboth@gmail.com>
2019-08-24 18:42:21 +0100

    Implement Spawners
    * Implement Spawners
    
    * Fix style issues

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    private int lastProbChance;
+
+    [Range(0, 1)]
+    public float probability;
+    public float spawnDelay;
+    public Enemy enemy;
+    public bool spawningEnabled;
+
+	// Use this for initialization
+	void Start () {
+        lastProbChance = (int) Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > lastProbChance + spawnDelay && spawningEnabled) {
+            Debug.Log("Chance " + probability);
+            if (Random.value < probability) {
+                Debug.Log("Spawning the enemy");
+                Instantiate(enemy, transform.position, Quaternion.identity);
+            }
+
+            lastProbChance = (int) Time.time;
+        }
+    }
+}
